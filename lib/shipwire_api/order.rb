e8772a604bf728587336a56606654b664ad71904
Hugh Kelsey <hugh@45robots.com>
2016-02-11 13:46:53 -0800

    able to paginate orders

--- lib/shipwire_api/order.rb
+++ lib/shipwire_api/order.rb
@@ -1,10 +1,36 @@
 module ShipwireAPI
-  class Order < ServiceRequest
+  class Orders < ServiceRequest
+
+    def orders
+      @orders ||= begin
+                    orders = []
+                    get
+                    orders << body['resource']['items']
+                    while next_url
+                      next_page
+                      orders << body['resource']['items']
+                    end
+                    orders.flatten.map {|o| Order.new(o)}
+                  end
+    end
+
 
     protected
 
     def resource_name
       'orders'
+    end
+  end
+
+  class Order
+    def initialize(params)
+      @params = params
+    end
+
+    private
+
+    def params
+      @params
     end
   end
 end
--- lib/shipwire_api/service_request.rb
+++ lib/shipwire_api/service_request.rb
@@ -2,26 +2,33 @@ require 'faraday'
 
 module ShipwireAPI
   class ServiceRequest
-    attr_reader :response, :errors
+    attr_reader :response, :errors, :next_url
 
     def initialize
       @errors = []
     end
 
-    def get
+    def get(url: ShipwireAPI.endpoint + resource_name, since: nil, limit: 100)
       begin
-        connection = Faraday.new(url: ShipwireAPI.endpoint + resource_name) do |faraday|
+        connection = Faraday.new(url: url) do |faraday|
           faraday.request :url_encoded
+          faraday.params['limit'] = limit
+          faraday.params['updatedAfter'] = since.utc.iso8601 if since.present?
           faraday.authorization "Basic", ShipwireAPI.authorization
           faraday.adapter Faraday.default_adapter
         end
-
         @response = connection.get
+        @next_url = body['resource']['next']
       rescue Faraday::ConnectionFailed
         @errors << 'Unable to connect to Shipwire'
       rescue
         @errors << 'Unknown error'
       end
+    end
+
+    def next_page
+      return if @next_url.nil?
+      get(url: next_url)
     end
 
     def is_ok?
@@ -34,12 +41,17 @@ module ShipwireAPI
     end
 
     protected
+
     def build_payload
       raise 'Override this'
     end
 
     def resource_name
       raise 'Override this'
+    end
+
+    def resource
+      body['resource']
     end
   end
 end
